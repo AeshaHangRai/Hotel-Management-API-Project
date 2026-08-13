d1ef661c759400fecd2ce45904d14f649620c318
AeshaHangRai <evilaesha@gmail.com>
2026-08-13 16:35:01 +0545

    Resolve merge conflict and update room API

--- Room/urls.py
+++ Room/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import RoomViewSet
 
 router = DefaultRouter()
-router.register(r'rooms', RoomViewSet, basename='room')
+router.register(r'Room', RoomViewSet, basename='Room')
 
 urlpatterns = router.urls
