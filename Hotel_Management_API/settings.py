222db9dd67b83de673f1a0282f23ea4bf34f8a7c
AeshaHangRai <evilaesha@gmail.com>
2026-08-13 22:31:41 +0545

    Update requirements.txt with simplejwt

--- Hotel_Management_API/settings.py
+++ Hotel_Management_API/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt.token_blacklist',
     'Room',
     'Hotel',
     'apps',
@@ -55,9 +56,9 @@ REST_FRAMEWORK = {
 }
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOEKN_LIFETIME': timedelta(days=7),
-    'ROTATE_REFRESH TOKENS' : True,
-    'BLACKLIST_AFTER_ROTATION' : True
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': True,
+    'BLACKLIST_AFTER_ROTATION': True,
 }
 
 MIDDLEWARE = [
